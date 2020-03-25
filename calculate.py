cea045f2e8c2737352dd56b66697f43d04beb18c
sifat hassan <sifat27.sh22@gmail.com>
2020-03-25 14:51:35 +0600

    calculate oparetions

---
+++ calculate.py
@@ -0,0 +1,9 @@
+print(2+2*3)
+print(2-2*3)
+print(2/2)#floating divison
+print(2//2)#integer divison
+print(2**3)#power
+print(round(2**0.5, 4))#  . befor number of floationg number
+print((2+3)*5/2%6)
+#5*5/2%6
+#25/2%6
