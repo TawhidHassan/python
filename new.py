371f1109cdf3e71a9b5c36266e5d3458eeb55389
sifat hassan <sifat27.sh22@gmail.com>
2020-03-28 14:07:12 +0600

    string indexing and formation reverse anay string

--- new.py
+++ new.py
@@ -1,8 +1,9 @@
-name="sifat"
-
+name=input("Enter you name ")
 # string indexing
 print(name[0])#1st latter
 print(name[-1])#1st latter
 print(name[0:2])#slicing ->more the two latter
 print(name[0:])#slicing ->more the two latter
 print(name[2:])#slicing ->more the two latter
+namex=name[-1::-1]
+print(f"you name reverse {namex}")#slicing ->more the two lattern with step arg
