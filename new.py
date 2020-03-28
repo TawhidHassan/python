dc65a773c8c179218b4eb075ecaed3a865c8a048
sifat hassan <sifat27.sh22@gmail.com>
2020-03-28 13:49:34 +0600

    String Indexing

--- new.py
+++ new.py
@@ -1,7 +1,9 @@
-name, age="sifat",int(22)
+name="sifat"
+age=22
 print("name: "+name+" age: "+str(age))
 
 name, age=input("enter your name and age ").split()
 
-print(name)
-print(age)
+# String formatting
+print("hello {} and your age is{}".format(name,age))#python 3
+print(f"hello {name} and your age is {int(age)+10}")#python 3.6
