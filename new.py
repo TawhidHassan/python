64c2865a3eaa754b9cfcd0ec8f5d32b22fd102da
sifat hassan <sifat27.sh22@gmail.com>
2020-03-29 15:05:01 +0600

    string some common uses functuion

--- new.py
+++ new.py
@@ -1,9 +1,14 @@
-name=input("Enter you name ")
-# string indexing
-print(name[0])#1st latter
-print(name[-1])#1st latter
-print(name[0:2])#slicing ->more the two latter
-print(name[0:])#slicing ->more the two latter
-print(name[2:])#slicing ->more the two latter
-namex=name[-1::-1]
-print(f"you name reverse {namex}")#slicing ->more the two lattern with step arg
+name="     sifat     "
+dot="..................."
+
+print(name.upper())
+print(name.count("i"))
+print(name.lstrip()+ dot)
+print(name.rstrip()+ dot)
+
+
+print(name.replace(" ","")+ dot)
+print(name.center(len(name)+8,"*"))
+
+
+
