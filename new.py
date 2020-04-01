60b9be7dd8c523c906f37f1dcdd3560ceb478550
sifat hassan <sifat27.sh22@gmail.com>
2020-04-01 15:07:46 +0600

    and, or oparator

--- new.py
+++ new.py
@@ -1,12 +1,22 @@
-age=input("enter you age: ")
-age=int(age)
+winingNum=20
+winingNumt=30
+userInput=input("enter you number b/w 1-100: ")
+userInput=int(userInput)
 
-if age>14:
-    print("your age is: "+str(age))
+#and oparator
+# if userInput>winingNum and winingNumt<userInput:
+#     print("you win!!")
+# else:
+#     if userInput<winingNum:
+#         print("too low number!!")
+#     else:
+#         print("too high")   
+
+#or oparator
+if userInput>winingNum or winingNumt<userInput:
+    print("you win!!")
 else:
-    print("sorry you age not requried")    
-
-if age>19:
-    pass #if ypu dont want to noting to write
- 
-      
+    if userInput<winingNum:
+        print("too low number!!")
+    else:
+        print("too high")   
