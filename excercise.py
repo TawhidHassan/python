154c7a7a603ddf9249b9d6d0e58cf3183ffb287f
sifat hassan <sifat27.sh22@gmail.com>
2020-04-02 13:38:04 +0600

    elif condetion

--- excercise.py
+++ excercise.py
@@ -4,6 +4,10 @@ userAge=int(userAge)
 
 if userAge>10 and (userName[0]=='a' or userName[0]=='A'):
     print("you can watch this movie!!")
+
+elif userAge==11:
+    print("user 11 year old")
+    
 else:
     print("you can not watch this movie!!")
    
