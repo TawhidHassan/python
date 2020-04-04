ac98f517e64ee45198d55de3fab33203f5bc653b
sifat hassan <sifat27.sh22@gmail.com>
2020-04-04 14:47:10 +0600

    beak,and continue function and excerise ->guess number game

--- excercise.py
+++ excercise.py
@@ -1,10 +1,20 @@
-userName=input("enter you name: ")
+winningNm=43
+guess=1
+number=int(input("gusess a number berween 1 nad 100: "))
+game_over=False
 
-i=0
-temp_var=""
+while not game_over:
+    if number==winningNm:
+        print(f"you wine :and you guesses this number in {guess} time")
+        game_over=True
+    else:    
+        if number<winningNm:
+          print("your guess low")
+          guess+=1
+          number=int(input("gusess a number again:  "))
+        else:    
+           print("your guess high")
+           guess+=1
+           number=int(input("gusess a number again:  "))
 
-while i<len(userName):
-    if userName[i] not in temp_var:
-        temp_var+=userName[i]
-        print(f"{userName[i]} : {userName.count(userName[i])}")
-    i+=1
+
--- new.py
+++ new.py
@@ -1,14 +1,15 @@
-# for loop
+# break use
 
 i=0
 n=int(input("enter you total sum number: "))
 total=0
-# for i in range(10):
-# for i in range(1,8):
-#     print(f"hello: {i}")
-
-
+# for i in range(1,n+1):
+#     if i==5:
+#         break
+#     total+=i
+#     print(i)
 for i in range(1,n+1):
+    if i==5:
+        continue
     total+=i
-
-print(total)
+    print(i)
