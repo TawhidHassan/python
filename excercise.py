9dcea0b82d5f8937ac915cde259ace08cae359f8
sifat hassan <sifat27.sh22@gmail.com>
2020-04-07 14:22:22 +0600

    use function print fibonacci number

--- excercise.py
+++ excercise.py
@@ -1,20 +1,22 @@
-winningNm=43
-guess=1
-number=int(input("gusess a number berween 1 nad 100: "))
-game_over=False
+#fibonacci series
+# 0 1 1 2 3 5 8 13 21
 
-while not game_over:
-    if number==winningNm:
-        print(f"you wine :and you guesses this number in {guess} time")
-        game_over=True
-    else:    
-        if number<winningNm:
-          print("your guess low")
-          guess+=1
-          number=int(input("gusess a number again:  "))
-        else:    
-           print("your guess high")
-           guess+=1
-           number=int(input("gusess a number again:  "))
+# for i in range(1,11):
+#   print(i, end =", ")
 
+def fibonacci(n):
+  a=0 #first number
+  b=1 #second number
+  if n==1:
+    print(a)
+  elif n==2:
+    print(a, b)
+  else:
+     print(a,b, end =" ")
+     for i in range(n-2):
+       c=a+b #c=1
+       a=b #a=1
+       b=c #b=1
+       print(b, end =" ")
 
+fibonacci(10)
