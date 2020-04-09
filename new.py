4902432a963ff66e08601168cc895965ed0d268c
sifat hassan <sifat27.sh22@gmail.com>
2020-04-09 14:16:12 +0600

    list add data on old list

--- new.py
+++ new.py
@@ -1,13 +1,8 @@
-# function
+# list
 
-def grater(a,b):
-    if a>b:
-        return a
-    else:
-        return b
+fruits=['grapes','apple']
 
-num1=int(input("enter 1 no: "))
-num2=int(input("enter 2 no: "))
-biger=grater(num1,num2)
+print(fruits)
 
-print(f"{biger} is greated")
+fruits.append('mang0')
+print(fruits)
