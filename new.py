cdae58019f384505da1c0842ccb87ce506826ea4
sifat hassan <sifat27.sh22@gmail.com>
2020-04-09 14:54:45 +0600

    delete data from list

--- new.py
+++ new.py
@@ -8,22 +8,7 @@
 # print(fruits)
 
 fruits=['grapes','apple']
-
+del fruits[1]
 print(fruits)
 
-# //add no of index
-fruits.insert(1,'mang0')
-print(fruits)
-fruits1=['xxx','ttt']
 
-# //join
-fruitss=fruits1+fruits
-print(fruitss)
-
-# extend
-fruitss.extend(fruits1)
-print(fruitss)
-
-# append
-fruitss.append(fruits1)
-print(fruitss)
