69f2393ce179e0ba20941946808fe6645699f996
sifat hassan <sifat27.sh22@gmail.com>
2020-04-09 14:49:43 +0600

    some list function

--- new.py
+++ new.py
@@ -1,8 +1,29 @@
 # list
 
+# fruits=['grapes','apple']
+
+# print(fruits)
+
+# fruits.append('mang0')
+# print(fruits)
+
 fruits=['grapes','apple']
 
 print(fruits)
 
-fruits.append('mang0')
+# //add no of index
+fruits.insert(1,'mang0')
 print(fruits)
+fruits1=['xxx','ttt']
+
+# //join
+fruitss=fruits1+fruits
+print(fruitss)
+
+# extend
+fruitss.extend(fruits1)
+print(fruitss)
+
+# append
+fruitss.append(fruits1)
+print(fruitss)
